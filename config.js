d15ae37826e8130fffd439adce455c8bc8044f0d
Sibu <95744973+Sibuuu07@users.noreply.github.com>
2022-04-15 17:36:47 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -22,8 +22,8 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['919337956084','16149454333,358753264940'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['919337956084'] //another owner number
+global.owner = ['919337956084','16149454333','358753264940'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['919337956084','16149454333'] //another owner number
 global.premium = ['919337956084'] //premium number
 global.pengguna = 'SIBU' //username
 global.botnma = '𝐀𝐑𝐂𝐓𝐈𝐗 𝐁𝐎𝐓' //bot name
